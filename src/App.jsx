b8b24b7c589ca8391038c114335d37c45815c7d7
badrabella-source <badr.abella@cuniv-tindouf.dz>
2025-12-13 13:21:47 -0800

    Initial commit - front end project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
-
-import Home from "./pages/Home";
-import Weather from "./pages/Weather";
-import News from "./pages/News";
-import History from "./pages/History";
-import Rest from "./pages/Rest";
-import Authentication from "./pages/Authentication";
-import Register from "./pages/Register";
-import Loginpage from "./pages/Loginpage";
-=======
 import { Home } from "./pages/Home";
 import { Weather } from "./pages/Weather";
 import { News } from "./pages/News";
@@ -20,34 +9,11 @@ import { Rest } from "./pages/Rest";
 import { Register } from "./pages/Register";
 import { Login } from "./pages/Login";
 import { SearchResults } from "./pages/SearchResults";
->>>>>>> 85317728ceaead571d64cb9a869248f340bd4890
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
-        {/* Home Page */}
-        <Route path="/" element={<Home />} />
-
-        {/* Other Pages */}
-        <Route path="/weather" element={<Weather />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/history" element={<History />} />
-        <Route path="/rest" element={<Rest />} />
-
-        {/* Authentication */}
-        <Route path="/auth" element={<Authentication />} />
-        <Route path="/login" element={<Loginpage />} />
-        <Route path="/register" element={<Register />} />
-
-        {/* Fallback: أي رابط غير موجود يرجع إلى Home */}
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-=======
         <Route path="/" element={<Home />} />
         <Route path="/weather" element={<Weather />} />
         <Route path="/news/:id" element={<News />} />
@@ -61,4 +27,3 @@ export default function App() {
     </BrowserRouter>
   );
 }
->>>>>>> 85317728ceaead571d64cb9a869248f340bd4890
