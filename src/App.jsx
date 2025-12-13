a50dbacab90e1d9ea8367f38127ad42ada91b9cd
badrabella-source <badr.abella@cuniv-tindouf.dz>
2025-12-13 13:25:04 -0800

    Initial commit - front end project

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Weather } from "./pages/Weather";
-import { News } from "./pages/News";
-import { History } from "./pages/History";
-import { Authentication } from "./pages/authentication";
-import { Rest } from "./pages/Rest";
-import { Register } from "./pages/Register";
-import { Login } from "./pages/Login";
-import { SearchResults } from "./pages/SearchResults";
+
+import Home from "./pages/Home";
+import Weather from "./pages/Weather";
+import News from "./pages/News";
+import History from "./pages/History";
+import Rest from "./pages/Rest";
+import Authentication from "./pages/Authentication";
+import Register from "./pages/Register";
+import Loginpage from "./pages/Loginpage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Home Page */}
         <Route path="/" element={<Home />} />
+
+        {/* Other Pages */}
         <Route path="/weather" element={<Weather />} />
-        <Route path="/news/:id" element={<News />} />
+        <Route path="/news" element={<News />} />
         <Route path="/history" element={<History />} />
-        <Route path="/auth" element={<Authentication />} />
         <Route path="/rest" element={<Rest />} />
+
+        {/* Authentication */}
+        <Route path="/auth" element={<Authentication />} />
+        <Route path="/login" element={<Loginpage />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/search-results" element={<SearchResults />} />
+
+        {/* Fallback: أي رابط غير موجود يرجع إلى Home */}
+        <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
